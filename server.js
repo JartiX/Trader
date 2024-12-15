d4a566bd28032220627a2e17ca8e6a89d1e64d48
JartiX <artempilavin@gmail.com>
2024-12-16 05:06:26 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('trust proxy', true);
 
 let savedResults = [];
 let ipRequestTimes = {};
